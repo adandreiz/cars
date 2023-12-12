497faeae73b2dbc9a0b9774a3316415d2aa02be5
Agustin D'Andreiz <agustin@contractsadvance.co.uk>
2023-12-12 15:52:19 +0000

    Refactored to use DTO

--- api/src/Service/CarService.php
+++ api/src/Service/CarService.php
@@ -2,18 +2,11 @@
 
 namespace App\Service;
 
+use App\Dto\CarDTO;
 use App\Entity\Car;
 use App\Entity\Colour;
-use App\Serializer\CarDenormalizer;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Exception\ExceptionInterface;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 class CarService
 {
@@ -29,37 +22,28 @@ class CarService
         $this->validationService = $validationService;
     }
 
-    public function createCar(string $carData): Car
+    public function createCar(CarDTO $dto): Car
     {
+        $car = new Car();
         try {
-            $encoder = new JsonEncoder();
-            $serializer = new Serializer(
-                [new DateTimeNormalizer(), new ObjectNormalizer(null, null, null, new ReflectionExtractor())],
-                [$encoder]
-            );
-            $car = $serializer->deserialize($carData, Car::class, 'json');
-            $serializer = new Serializer([new CarDenormalizer($this->doctrine->getRepository(Colour::class))], [$encoder]);
-            $serializer->deserialize(
-                $carData,
-                Car::class,
-                'json',
-                [
-                    AbstractNormalizer::OBJECT_TO_POPULATE => $car
-                ]
-            );
-        } catch (ExceptionInterface $e) {
+            $colour = $this->doctrine->getRepository(Colour::class)->findOrFail($dto->colourId);
+        } catch (ExceptionService $exceptionData) {
             $exceptionData = new ExceptionDataService(
                 JsonResponse::HTTP_UNPROCESSABLE_ENTITY,
-                $e->getMessage()
+                $exceptionData->getMessage()
             );
             throw new ExceptionService($exceptionData);
         }
 
+        $car->setBuildDate(new \DateTimeImmutable($dto->buildDate))
+            ->setModel($dto->model)
+            ->setMake($dto->make)
+            ->setColour($colour);
         $this->validationService->validateEntity($car);
-
         $em = $this->doctrine->getManager();
         $em->persist($car);
         $em->flush();
+
         return $car;
     }
 
